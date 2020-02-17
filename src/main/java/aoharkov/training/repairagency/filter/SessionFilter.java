d8defe56dde9c0ec394db88dc41a9056fe2b2119
Artem Oharkov <aoharkov@kse.org.ua>
2020-02-17 10:48:06 +0200

    add SessionFilter

---
+++ src/main/java/aoharkov/training/repairagency/filter/SessionFilter.java
@@ -0,0 +1,21 @@
+package aoharkov.training.repairagency.filter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class SessionFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        if (request.getSession(false) == null) {
+            request.getSession(true);
+        }
+        chain.doFilter(request, servletResponse);
+    }
+}
