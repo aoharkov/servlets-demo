8573c4a7eb3a3b75d104f1c3463483376ca04f79
Artem Oharkov <aoharkov@kse.org.ua>
2020-02-29 20:59:11 +0200

    finish testing of client service

--- src/test/java/aoharkov/training/repairagency/service/impl/ClientServiceImplTest.java
+++ src/test/java/aoharkov/training/repairagency/service/impl/ClientServiceImplTest.java
@@ -24,9 +24,12 @@ import aoharkov.training.repairagency.mapper.RepairStageMapper;
 import aoharkov.training.repairagency.mapper.RequestMapper;
 import aoharkov.training.repairagency.mapper.UserMapper;
 import aoharkov.training.repairagency.service.encoder.Encoder;
+import aoharkov.training.repairagency.service.exception.EntityNotFoundException;
 import aoharkov.training.repairagency.service.validator.Validator;
 import org.junit.After;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -34,8 +37,11 @@ import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.Optional;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
@@ -49,15 +55,17 @@ public class ClientServiceImplTest {
     private static final RequestEntity REQUEST_ENTITY = initRequestEntity();
     private static final Request REQUEST = initRequest();
     private static final Page<RequestEntity> REQUEST_ENTITY_PAGE = initRequestEntityPage();
-    private static final Page<Request> REQUEST_PAGE = initRequestPage();
+    private static final RepairStageEntity REPAIR_STAGE_ENTITY = initRepairStageEntity();
+    private static final RepairStage REPAIR_STAGE = initRepairStage();
     private static final OrderEntity ORDER_ENTITY = initOrderEntity();
     private static final Order ORDER = initOrder();
     private static final RefusalEntity REFUSAL_ENTITY = initRefusalEntity();
     private static final Refusal REFUSAL = initRefusal();
     private static final FeedbackEntity FEEDBACK_ENTITY = initFeedbackEntity();
     private static final Feedback FEEDBACK = initFeedback();
-    private static final RepairStageEntity REPAIR_STAGE_ENTITY = initRepairStageEntity();
-    private static final RepairStage REPAIR_STAGE = initRepairStage();
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
 
     @Mock
     private Validator userValidator;
@@ -109,16 +117,11 @@ public class ClientServiceImplTest {
                 .build();
     }
 
-    private static Page<Request> initRequestPage() {
-        return Page.<Request>builder()
-                .withContent(Collections.emptyList())
-                .build();
-    }
-
     private static OrderEntity initOrderEntity() {
         return OrderEntity.builder()
                 .withId(2)
                 .withRequestId(1)
+                .withRepairStageId(5)
                 .build();
     }
 
@@ -126,6 +129,7 @@ public class ClientServiceImplTest {
         return Order.builder()
                 .withId(2)
                 .withRequest(REQUEST)
+                .withRepairStage(REPAIR_STAGE)
                 .build();
     }
 
@@ -185,20 +189,6 @@ public class ClientServiceImplTest {
 
         verify(requestMapper).mapDomainToEntity(eq(REQUEST));
         verify(requestDao).save(eq(REQUEST_ENTITY));
-        verifyZeroInteractions(userValidator);
-        verifyZeroInteractions(passwordEncoder);
-        verifyZeroInteractions(userDao);
-        verifyZeroInteractions(userMapper);
-        //verifyZeroInteractions(requestDao);
-        verifyZeroInteractions(refusalDao);
-        verifyZeroInteractions(orderDao);
-        verifyZeroInteractions(repairStageDao);
-        verifyZeroInteractions(feedbackDao);
-        //verifyZeroInteractions(requestMapper);
-        verifyZeroInteractions(refusalMapper);
-        verifyZeroInteractions(orderMapper);
-        verifyZeroInteractions(repairStageMapper);
-        verifyZeroInteractions(feedbackMapper);
     }
 
     @Test
@@ -206,39 +196,115 @@ public class ClientServiceImplTest {
         when(requestDao.findAllByClientId(anyInt(), anyInt(), anyInt())).thenReturn(REQUEST_ENTITY_PAGE);
         when(requestMapper.mapEntityToDomain(eq(REQUEST_ENTITY))).thenReturn(REQUEST);
 
-        clientService.findOwnRequests(1, 5, 1);
+        assertThat(clientService.findOwnRequests(1, 5, 1).getContent(),
+                equalTo(Arrays.asList(REQUEST, REQUEST)));
 
         verify(requestDao).findAllByClientId(anyInt(), anyInt(), anyInt());
         verify(requestMapper, times(2)).mapEntityToDomain(eq(REQUEST_ENTITY));
-        verifyZeroInteractions(userValidator);
-        verifyZeroInteractions(passwordEncoder);
-        verifyZeroInteractions(userDao);
-        verifyZeroInteractions(userMapper);
-        //verifyZeroInteractions(requestDao);
-        verifyZeroInteractions(refusalDao);
-        verifyZeroInteractions(orderDao);
-        verifyZeroInteractions(repairStageDao);
-        verifyZeroInteractions(feedbackDao);
-        //verifyZeroInteractions(requestMapper);
+    }
+
+    @Test
+    public void findRequestShouldFindSuccessfully() {
+        when(requestDao.findById(eq(1))).thenReturn(Optional.of(REQUEST_ENTITY));
+        when(requestMapper.mapEntityToDomain(eq(REQUEST_ENTITY))).thenReturn(REQUEST);
+
+        assertEquals(REQUEST, clientService.findRequest(1));
+
+        verify(requestDao).findById(eq(1));
+        verify(requestMapper).mapEntityToDomain(eq(REQUEST_ENTITY));
+    }
+
+    @Test
+    public void findRequestShouldThrowEntityNotFoundException() {
+        when(requestDao.findById(eq(99))).thenReturn(Optional.empty());
+
+        exception.expect(EntityNotFoundException.class);
+        clientService.findRequest(99);
+
+        verify(requestDao).findById(eq(99));
+        verifyZeroInteractions(requestMapper);
+    }
+
+    @Test
+    public void findOrderShouldFindSuccessfully() {
+        when(orderDao.findByRequestId(eq(1))).thenReturn(Optional.of(ORDER_ENTITY));
+        when(orderMapper.mapEntityToDomain(eq(ORDER_ENTITY))).thenReturn(ORDER);
+
+        assertEquals(ORDER, clientService.findOrder(1));
+
+        verify(orderDao).findByRequestId(eq(1));
+        verify(orderMapper).mapEntityToDomain(eq(ORDER_ENTITY));
+    }
+
+    @Test
+    public void findOrderShouldThrowEntityNotFoundException() {
+        when(orderDao.findByRequestId(eq(99))).thenReturn(Optional.empty());
+
+        exception.expect(EntityNotFoundException.class);
+        clientService.findOrder(99);
+
+        verify(orderDao).findByRequestId(eq(99));
+        verifyZeroInteractions(requestMapper);
+    }
+
+    @Test
+    public void findRefusalShouldFindSuccessfully() {
+        when(refusalDao.findByRequestId(eq(1))).thenReturn(Optional.of(REFUSAL_ENTITY));
+        when(refusalMapper.mapEntityToDomain(eq(REFUSAL_ENTITY))).thenReturn(REFUSAL);
+
+        assertEquals(REFUSAL, clientService.findRefusal(1));
+
+        verify(refusalDao).findByRequestId(eq(1));
+        verify(refusalMapper).mapEntityToDomain(eq(REFUSAL_ENTITY));
+    }
+
+    @Test
+    public void findRefusalShouldThrowEntityNotFoundException() {
+        when(refusalDao.findByRequestId(eq(99))).thenReturn(Optional.empty());
+
+        exception.expect(EntityNotFoundException.class);
+        clientService.findRefusal(99);
+
+        verify(refusalDao).findByRequestId(eq(99));
         verifyZeroInteractions(refusalMapper);
+    }
+
+    @Test
+    public void saveFeedbackShouldSaveSuccessfully() {
+        when(feedbackMapper.mapDomainToEntity(eq(FEEDBACK))).thenReturn(FEEDBACK_ENTITY);
+        doNothing().when(feedbackDao).save(FEEDBACK_ENTITY);
+
+        clientService.saveFeedback(FEEDBACK);
+
+        verify(feedbackMapper).mapDomainToEntity(eq(FEEDBACK));
+        verify(feedbackDao).save(eq(FEEDBACK_ENTITY));
+    }
+
+    @Test
+    public void findRepairStageShouldFindSuccessfully() {
+        when(orderDao.findByRequestId(eq(1))).thenReturn(Optional.of(ORDER_ENTITY));
+        when(orderMapper.mapEntityToDomain(eq(ORDER_ENTITY))).thenReturn(ORDER);
+        when(repairStageDao.findById(eq(5))).thenReturn(Optional.of(REPAIR_STAGE_ENTITY));
+        when(repairStageMapper.mapEntityToDomain(eq(REPAIR_STAGE_ENTITY))).thenReturn(REPAIR_STAGE);
+
+        assertEquals(REPAIR_STAGE, clientService.findRepairStage(1));
+
+        verify(orderDao).findByRequestId(eq(1));
+        verify(orderMapper).mapEntityToDomain(eq(ORDER_ENTITY));
+        verify(repairStageDao).findById(eq(5));
+        verify(repairStageMapper).mapEntityToDomain(eq(REPAIR_STAGE_ENTITY));
+    }
+
+    @Test
+    public void findRepairStageShouldThrowEntityNotFoundException() {
+        when(orderDao.findByRequestId(eq(99))).thenReturn(Optional.empty());
+
+        exception.expect(EntityNotFoundException.class);
+        clientService.findRepairStage(99);
+
+        verify(orderDao).findByRequestId(eq(99));
         verifyZeroInteractions(orderMapper);
+        verifyZeroInteractions(repairStageDao);
         verifyZeroInteractions(repairStageMapper);
-        verifyZeroInteractions(feedbackMapper);
-    }
-
-    @Test
-    public void findOrder() {
-    }
-
-    @Test
-    public void findRefusal() {
-    }
-
-    @Test
-    public void saveFeedback() {
-    }
-
-    @Test
-    public void findRepairStage() {
     }
 }
