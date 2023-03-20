7fbdb78a9a95af722dcf7032ad7ca513310ef2f4
Artem Oharkov <aoharkov@kse.org.ua>
2023-03-20 13:42:57 +0200

    test CodeClimate with false duplication

---
+++ src/main/java/aoharkov/training/repairagency/entity/NewEntity.java
@@ -0,0 +1,101 @@
+package aoharkov.training.repairagency.entity;
+
+import java.util.Objects;
+
+public class NewEntity {
+    private final Integer id;
+    private final Integer numericValue;
+    private final String text;
+    private final Integer anotherNumericValue;
+
+    private NewEntity(Builder builder) {
+        this.id = builder.id;
+        this.numericValue = builder.numericValue;
+        this.text = builder.text;
+        this.anotherNumericValue = builder.anotherNumericValue;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Integer getNumericValue() {
+        return numericValue;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Integer getAnotherNumericValue() {
+        return anotherNumericValue;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof NewEntity)) {
+            return false;
+        }
+        NewEntity that = (NewEntity) o;
+        return id.equals(that.id) &&
+                Objects.equals(numericValue, that.numericValue) &&
+                Objects.equals(text, that.text) &&
+                Objects.equals(anotherNumericValue, that.anotherNumericValue);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, numericValue, text, anotherNumericValue);
+    }
+
+    @Override
+    public String toString() {
+        return "New entity{" +
+                "id=" + id +
+                ", numericValue=" + numericValue +
+                ", text='" + text + '\'' +
+                ", anotherNumericValue=" + anotherNumericValue +
+                '}';
+    }
+
+    public static final class Builder {
+        private Integer id;
+        private Integer numericValue;
+        private String text;
+        private Integer anotherNumericValue;
+
+        private Builder() {
+        }
+
+        public Builder withId(Integer id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder withNumericValue(Integer requestId) {
+            this.numericValue = requestId;
+            return this;
+        }
+
+        public Builder withText(String text) {
+            this.text = text;
+            return this;
+        }
+
+        public Builder withAnotherNumericValue(Integer score) {
+            this.anotherNumericValue = score;
+            return this;
+        }
+
+        public NewEntity build() {
+            return new NewEntity(this);
+        }
+    }
+}
